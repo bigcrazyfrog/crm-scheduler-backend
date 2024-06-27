d1939a637ba323fa8726896cfd2d837050b6667a
Nikita <90467228+bigcrazyfrog@users.noreply.github.com>
2024-06-28 01:40:51 +0500

    Check payments (#18)
    - Chech payment periodicly
    - Add celery task

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'config.settings')
 
@@ -9,3 +10,11 @@ app = Celery('scheduler')
 app.config_from_object('django.conf:settings', namespace="CELERY")
 
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'every': { 
+        'task': '<name_of_app>.tasks.repeat_order_make',
+        'schedule': crontab(minute=5),
+    }
+}
